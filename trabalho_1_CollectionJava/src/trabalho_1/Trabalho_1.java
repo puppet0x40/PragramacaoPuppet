5af1751a51bf90ca2e8d9d50bfcbffb72a976b9b
puppet0x40 <https://github.com/puppet0x40/TAD.git>
2019-05-27 10:37:38 -0300

    Update Trabalho_1.java

--- trabalho_1_CollectionJava/src/trabalho_1/Trabalho_1.java
+++ trabalho_1_CollectionJava/src/trabalho_1/Trabalho_1.java
@@ -14,8 +14,8 @@ public class Trabalho_1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+    */
 }
