0818a8eb10699b95ef397a5224fe29375627bd0e
puppet0x40 <https://github.com/puppet0x40/TAD.git>
2019-05-27 13:44:44 -0300

    Create Main.java

---
+++ trabalho_2_heranca_interface/src/trabalho_2/Main.java
@@ -0,0 +1,45 @@
+
+package trabalho_2;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        Televisao tv = new Televisao("toshiba ", "S4H", 1234);
+        Receptor receptor = new Receptor();
+        //int opcao;
+        
+        tv.setCanal(3);
+        tv.setVolume(50);
+       
+        
+        receptor.setMarca("oi livre");
+        receptor.setModelo("OIHDfull");
+        receptor.setNumeroDeSerie(458422);
+        receptor.setCanal(200);
+        receptor.setVolume(80);
+        
+        tv.informacao();
+        
+        tv.OnOff();
+        tv.aumentaVolune();
+        tv.aumentaVolune();
+        tv.aumentaVolune();
+        tv.aumentaVolune();
+        tv.proximoCanal();
+        tv.voltarCanal();
+        tv.OnOff();
+       
+        
+        receptor.informacao();
+       
+        receptor.OnOff();
+        receptor.aumentaVolune();
+        receptor.proximoCanal();
+        receptor.voltarCanal();
+        receptor.OnOff();
+       
+    }
+    
+}
